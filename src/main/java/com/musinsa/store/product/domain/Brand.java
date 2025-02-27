de3ec764eb850895f4554e1b7854c5b036463c43
sungtaek <leesungtaek@gmail.com>
2025-02-28 00:53:14 +0900

    add category check

--- src/main/java/com/musinsa/store/product/domain/Brand.java
+++ src/main/java/com/musinsa/store/product/domain/Brand.java
@@ -1,18 +1,21 @@
 package com.musinsa.store.product.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.Builder.Default;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Brand {
   private Long id;
   private String name;
@@ -28,5 +31,13 @@ public class Brand {
           .sum();
     }
     return totalPrice;
+  }
+
+  public boolean hasAllCategories() {
+    HashSet<Category> marks = new HashSet<>(List.of(Category.values()));
+    products.stream()
+        .map(Product::getCategory)
+        .forEach(marks::remove);
+    return marks.isEmpty();
   }
 }
