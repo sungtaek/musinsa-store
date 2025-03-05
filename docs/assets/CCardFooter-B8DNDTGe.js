c9842e315c2305576902addb90b00cd88ae16e2c
sungtaek <leesungtaek@gmail.com>
2025-03-05 12:43:34 +0900

    update

--- docs/assets/CCardFooter-B8DNDTGe.js
+++
@@ -1,1 +0,0 @@
-import{a as l,_ as d,R as n,b as i,c as m,P as e}from"./index-vWEjdDY1.js";var s=l.forwardRef(function(a,r){var c=a.children,o=a.className,t=d(a,["children","className"]);return n.createElement("div",i({className:m("card-footer",o)},t,{ref:r}),c)});s.propTypes={children:e.node,className:e.string};s.displayName="CCardFooter";export{s as C};
