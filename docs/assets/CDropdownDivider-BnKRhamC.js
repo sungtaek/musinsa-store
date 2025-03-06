f8143f98e40f69a4395e2910b7a26e83677f17f9
sungtaek <leesungtaek@gmail.com>
2025-03-06 21:16:33 +0900

    delete old

--- docs/assets/CDropdownDivider-BnKRhamC.js
+++
@@ -1,1 +0,0 @@
-import{a as p,_ as t,R as c,b as i,c as d,P as m}from"./index-mjNz_a55.js";var a=p.forwardRef(function(s,r){var e=s.className,o=t(s,["className"]);return c.createElement("hr",i({className:d("dropdown-divider",e)},o,{ref:r}))});a.propTypes={className:m.string};a.displayName="CDropdownDivider";export{a as C};
